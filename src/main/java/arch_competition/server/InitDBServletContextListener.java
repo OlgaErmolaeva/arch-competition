d3a4ee92dd345885ccf9f35873e52ffd56a29b7a
Olga Ermolaeva <ermololya@gmail.com>
2016-04-13 21:33:05 +0200

    - fixed ddl query

--- src/main/java/arch_competition/server/InitDBServletContextListener.java
+++ src/main/java/arch_competition/server/InitDBServletContextListener.java
@@ -22,9 +22,12 @@ public class InitDBServletContextListener implements ServletContextListener {
         try (final Connection connection = DriverManager.getConnection(ServerConstants.JDBC_URL, ServerConstants.USER, ServerConstants.PASSWORD);
              final Statement statement = connection.createStatement()) {
             statement.executeUpdate(
-                    "CREATE TABLE if not exists 'design_projects' " +
-                            "('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' VARCHAR(255), 'description' VARCHAR(3000), " +
-                            "'creation_date' DATE, 'picture' VARCHAR(255))");
+                    "CREATE TABLE if not exists design_projects " +
+                            "(id INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                            "name VARCHAR(255), " +
+                            "description VARCHAR(3000), " +
+                            "creation_date DATE, " +
+                            "picture VARCHAR(255))");
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
